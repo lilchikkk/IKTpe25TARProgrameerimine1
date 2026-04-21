09160ea0769722deb65186b9e69c1ba4a69061cd
Opilane TTHK <opilane@tthk.ee>
2026-04-21 14:06:57 +0300

    Create

--- University/University/Controllers/StudentController.cs
+++ University/University/Controllers/StudentController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using University.Data;
+using University.Models;
+using University.ViewModel;
 
 namespace University.Controllers
 {
@@ -45,6 +47,13 @@ namespace University.Controllers
             var student = await _context.Students
                 .FirstOrDefaultAsync(m => m.Id == id);
 
+            var vm = new ViewModel.StudentDetailsViewModel
+            {
+                Id = student.Id,
+                LastName = student.FirstMidName,
+                EnrollmentDate = student.EnrollmentDate
+            };
+
             //miks me kasutame ToListAsync?
             //kui me kasutame see, siis me saame tulemuse listina
 
@@ -55,7 +64,41 @@ namespace University.Controllers
             }
 
             //kui student on leitud, siis tagastame tulemus 
-            return View(student);
+            return View(vm);
+        }
+
+        //GET : student/create
+        //see meetod tagastab vaate, kus saab luua uue studenti
+        public IActionResult  Create()
+        {
+            return View();
+        }
+        //Post : STudent/create
+        //seemetod salvestab uue studenti andmebaasi
+        [HttpPost]
+        //see meetod on kaitstud CSRF rünnakutte eest
+        //see meetod on askroonene, mis tähendab, et see metod ei saa
+        //olaa samaaegselt mitu kordakäivitatud
+        public async Task<IActionResult> Create(StudentCreateViewModel vm)
+        {
+            //kui model on valiidnesiis loome uue studenti ja salvetame selle andmebaasi
+            if (ModelState.IsValid)
+            {
+                var student = new Models.Student
+                {
+                    LastName = vm.LastName,
+                    FirstMidName = vm.FirstMidName,
+                    EnrollmentDate = vm.EnrollmentDate
+                };
+                //liisame studentis andmebaasi ja salvestame muudatused
+                _context.Add(student);
+                //miks kasutame await?
+                //kui me kasutame await, siiis me ootame kuni salvestamine on lõpetatud
+                await _context.SaveChangesAsync();
+                //pärast salvestamist suuname kasutaja tagasiIndex vaatese
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vm);
         }
     }
 }
