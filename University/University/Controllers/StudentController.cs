c8ea7f6b99bb14d6ec61ed2dc621a1ec4d4f2405
Opilane TTHK <opilane@tthk.ee>
2026-04-23 14:56:32 +0300

    update fix

--- University/University/Controllers/StudentController.cs
+++ University/University/Controllers/StudentController.cs
@@ -133,12 +133,6 @@ namespace University.Controllers
         public async Task<IActionResult> Update(int id)
         {
             var student = await _context.Students
-               .Include(s => s.Enrollments)
-               //Kui tahad uuesti objeti kasutada objekti sees, siis kasutad ThenInclude
-               .ThenInclude(e => e.Course)
-               //andmeid ei salvestata vähemällu ja ei jälgita
-               .AsNoTracking()
-               // Leiab esimese elemendi andmetes, mis on tingimuse välja toodud 
                .FirstOrDefaultAsync(m => m.Id == id);
 
 
