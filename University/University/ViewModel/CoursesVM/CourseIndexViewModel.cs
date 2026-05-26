fe51f7d9dcd7fbe2f4ce0a1386d7e28b0d95957f
Opilane TTHK <opilane@tthk.ee>
2026-05-26 15:56:19 +0300

    Course new adding

---
+++ University/University/ViewModel/CoursesVM/CourseIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using University.Models;
+
+namespace University.ViewModel
+{
+    public class CourseIndexViewModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
--- University/University/ViewModel/StudentIndexViewModel.cs
+++ University/University/ViewModel/StudentIndexViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace University.ViewModel
 {
     public class StudentIndexViewModel
